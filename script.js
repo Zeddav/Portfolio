ce9f672844b6b738cc238ac4250fd5de508aa66e
Zelie Davaud <zdavaud@iutbayonne.univ-pau.fr>
2024-12-05 11:15:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemple de script JS pour ajouter des animations ou autres fonctionnalités à l'avenir
+// Exemple : afficher un message de bienvenue lors du chargement de la page
+window.onload = function() {
+    alert('Bienvenue sur mon portfolio!');
+}
